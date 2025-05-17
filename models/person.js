4c6cf106034175d7f44302eb21b132a92f9b2f85
JohnasonY <157220107+JohnasonY@users.noreply.github.com>
2025-05-17 14:49:42 -0400

    Update person.js

--- models/person.js
+++ models/person.js
@@ -4,40 +4,40 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-console.log('connecting to', url);
+console.log('connecting to', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB');
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message);
-    })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /^\d{2,3}-\d+$/.test(v)
+      },
+      message: () => 'Must be in the form of xx-xxxxx... or xxx-xxxx...'
     },
-    number: {
-        type: String,
-        validate: {
-            validator: function(v) {
-                return /^\d{2,3}-\d+$/.test(v)
-            },
-            message: props => `Must be in the form of xx-xxxxx... or xxx-xxxx...`
-        },
-        minLength: 8,
-        required: true
-    },
+    minLength: 8,
+    required: true
+  },
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', personSchema)
